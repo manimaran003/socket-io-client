4fd13d66b9773c3a4a52cbe0e3c8721ff88b7c4f
Github@1999 <manimaranc@dorustree.in>
2022-04-02 15:58:28 +0530

    socket-io-changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
 import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client'
+import './App.css'
 import Chat from './Chat'
-const App=()=>{
-  let socket=io.connect("http://localhost:3001")
-  const [state,setState]=useState({
-    username:"",
-    room:""
+import {toast,ToastContainer} from 'react-toastify'
+const App = () => {
+  let socket = io.connect("http://localhost:3001")
+  const [state, setState] = useState({
+    username: "",
+    room: ""
   })
-  const handleChange=(e)=>{
-    const {name,value}=e.target
-    setState({...state,[name]:value})
+  const [show,setShow]=useState(false)
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState({ ...state, [name]: value })
   }
-  const joinRoom=(e)=>{
+  const joinRoom = (e) => {
     e.preventDefault()
-    if(state.username!=="" &&state.room!==""){
-      socket.emit("join_room",state.room)
+    if (state.username !== "" && state.room !== "") {
+      socket.emit("join_room", state.room)
+      setShow(true)
+      setTimeout(()=>{
+        setShow(false)
+      },4000)
     }
   }
-  useEffect(()=>{
-    socket.on("received_message",(data)=>{
-      console.log("recieved",data)
-    })
-  },[socket])
   return (
     <>
-    <div>
-      <input name="username" value={state.username} onChange={handleChange}/>
-      <input  name="room" value={state.room} onChange={handleChange}/>
-      <button onClick={joinRoom}>Join</button>
-      <Chat name={state.username} room={state.room} socket={socket}/>
-    </div>
+      <div className="main-header">
+        <div className='container'>
+          <div className='d-flex justify-content-center mt-3'>
+            <h2>Chat Application</h2>
+          </div>
+          {show && <p>{state.username} joined in group</p>}
+          <div className='row mt-4'>
+            <div className="col-6">
+              <form>
+                <label htmlFor="exampleFormControlInput1" className="form-label">username</label>
+                <div className="input-group mb-3">
+                  <span className="input-group-text" id="basic-addon1">@</span>
+                  <input type="text" name="username" value={state.username} onChange={handleChange} className="form-control" placeholder="Username" id="exampleFormControlInput1" />
+
+                </div>
+                <label className="form-label">room</label>
+                <div className="input-group mb-3">
+                  <span className="input-group-text" id="basic-addon1">@</span>
+                  <input type="text" name="room" value={state.room} onChange={handleChange} className="form-control " placeholder="room" aria-label="Username" id="exampleFormControlInput2" aria-describedby="basic-addon1" />
+
+                </div>
+
+                <div className="d-flex justify-content-center mt-3">
+                  <button type="button" onClick={joinRoom} className="btn btn-lg btn-primary login-btn">Join</button>
+                </div>
+              </form>
+            </div>
+            <div className="col-6">
+              <Chat name={state.username} room={state.room} socket={socket}/>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-
+import React, { useState,useEffect} from "react";
 const Chat=(props)=>{
     const{name,room,socket}=props
     const[chats,setChats]=useState([])
+    const [messageList,setmessageList]=useState([])
     const handleChange=(e)=>{
         setChats(e.target.value)
     }
-    const handleSubmit=(e)=>{
+    const handleSubmit=async(e)=>{
         e.preventDefault()
         if(chats!==""){
             let messageData={
@@ -14,17 +14,52 @@ const Chat=(props)=>{
                 author:name,
                 message:chats,
             }
-            socket.emit("send_message",messageData)
+            await socket.emit("send_message",messageData)
+            //setmessageList((list)=>[...list,messageData])
+            setChats("")
         }
     }
+    useEffect(() => {
+        socket.on("received_message", (data) => {
+          console.log("recieved from  clients", data)
+          setmessageList((list)=>[...list,data])
+        })
+      }, [socket])
+      console.log(messageList)
     return (
         <>
         <div>
             <h2>live chat</h2>
-            <div>
+            <div className='d-flex justify-content-center'>
+            <div className="card" style={{ width: "30rem" }}>
+              <div className="card-body justify-content-center d-flex">
+                <h5 className="card-title">Chat App</h5>
+              </div>
+              <div className="card" style={{ width: "30rem", height: "30rem" }}>
+                  {
+                      messageList.map((item)=>{
+                          return (
+                              <>
+                              <div style={{width:"100%"}}>
+                              <div className={name===item.author?"msgLeft":"msgRight"}>
+                                <div style={{width:'100px',background:`${name===item.author?"red":"green"}`}}>
+                                <p> {item.message}</p>
+                                    </div>
+                              </div>
+                              </div>
+                              </>
+                          )
+                      })
+                  }
+              </div>
+              <div className="card-body d-flex justify-content-end">
+              <div>
                 <input placeholder="hey" onChange={handleChange} name="chats" value={chats}/>
                 <button onClick={handleSubmit}>send</button>
             </div>
+              </div>
+            </div>
+          </div>
         </div>
         </>
     )
